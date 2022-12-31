14117b0e75840d0b87928ff935289cf266b1f149
Tolu Olugbesan <tolugbesan@gmail.com>
2022-12-31 06:42:43 +0100

    Migrate to nextjs 13

---
+++ frontend-next/app/layout.tsx
@@ -0,0 +1,17 @@
+import "../styles/globals.css";
+import Head from "./head";
+
+export default function RootLayout({
+    // Layouts must accept a children prop.
+    // This will be populated with nested layouts or pages
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    return (
+      <html lang="en">
+        <Head />
+        <body>{children}</body>
+      </html>
+    );
+  }
---
+++ frontend-next/app/page.tsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+      <div>
+        <p className="text-3xl font-bold text-green-600">Currently dressing the Earth</p>
+      </div>
+    )
+}
+
+export default Home;
