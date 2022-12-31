fbe30101f2f03b069f3dae5de59535a3efe62138
Tolu Olugbesan <tolugbesan@gmail.com>
2022-12-31 07:03:20 +0100

    Add JIT config

--- frontend-next/tailwind.config.js
+++ frontend-next/tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./app/**/*.{js,ts,jsx,tsx}",
   ],
+  // darkMode: false,
   theme: {
     extend: {},
   },
