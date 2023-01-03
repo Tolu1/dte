63463c8826e3acd97a65ec512c117cd985c4b83b
Tolu Olugbesan <tolugbesan@gmail.com>
2023-01-03 15:51:35 +0100

    Add hover effect

--- frontend-next/app/ConnectWallet.tsx
+++ frontend-next/app/ConnectWallet.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function ConnectWallet() {
   return (
-    <div className="p-2 text-white bg-green rounded-xl text-sm">
+    <div className="p-2 px-6 pt-2 text-white bg-green rounded-xl text-sm hover:bg-green-hover">
       <Link href="#">Connect Wallet</Link>
     </div>
   )
