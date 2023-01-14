a50a838cd56d834bad55f9a6b45e4cd663053469
Tolu Olugbesan <tolugbesan@gmail.com>
2023-01-14 14:58:47 +0100

    Update UI and code formatting

--- frontend-next/app/NavbarHamburger.tsx
+++ frontend-next/app/NavbarHamburger.tsx
@@ -1,32 +1,39 @@
-import React from 'react'
-import ConnectWallet from './ConnectWallet'
+import React from "react";
+import ConnectWallet from "./ConnectWallet";
 
 type NavbarHamburgerItemProps = {
   label: string;
   href: string;
-}
+};
 
-const NavbarHamburgerItem = ({label, href}: NavbarHamburgerItemProps) => {
+const NavbarHamburgerItem = ({ label, href }: NavbarHamburgerItemProps) => {
   return (
-    <a href={href} title="" className="flex items-center p-3 -m-3 text-base font-medium text-gray-900 transition-all duration-200 rounded-xl hover:bg-gray-50 focus:outline-none font-pj focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"> {label} </a>
-  )
-}
+    <a
+      href={href}
+      title=""
+      className="flex items-center p-3 -m-3 text-base font-medium text-gray-900 transition-all duration-200 rounded-xl hover:bg-gray-50 focus:outline-none font-pj focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"
+    >
+      {" "}
+      {label}{" "}
+    </a>
+  );
+};
 
 function NavbarHamburger() {
   return (
     <nav>
-        <div className="px-1 py-8">
-            <div className="grid gap-y-7">
-              <NavbarHamburgerItem label="Causes" href="/causes" />
-              <NavbarHamburgerItem label="Shop" href="#" />
-              <NavbarHamburgerItem label="Community" href="#" />
-              <NavbarHamburgerItem label="About" href="#" /> 
-              <NavbarHamburgerItem label="Dashboard" href="#" /> 
-              <ConnectWallet />
-            </div>
+      <div className="px-1 py-8">
+        <div className="grid gap-y-7">
+          <NavbarHamburgerItem label="Causes" href="/causes" />
+          <NavbarHamburgerItem label="Shop" href="#" />
+          <NavbarHamburgerItem label="Community" href="#" />
+          <NavbarHamburgerItem label="About" href="#" />
+          <NavbarHamburgerItem label="Dashboard" href="#" />
+          <ConnectWallet />
         </div>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default NavbarHamburger
+export default NavbarHamburger;
--- frontend-next/app/layout.tsx
+++ frontend-next/app/layout.tsx
@@ -4,20 +4,21 @@ import AppProvider from "../context/AppContext";
 import Footer from "./Footer";
 import Header from "./Header";
 
-export default function RootLayout({ children, }: { children: React.ReactNode; }) {
-    return (
-      <html lang="en">
-        <body className="font-inter">
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="font-inter">
         <AppProvider>
           <Header />
-          // Workaround for fixed header block hero content, added mt-12 to the section element class
-          {/* <div className="mt-12"></div> */}
-          <div className="mt-11">
-            {children}
-          </div>
+          {/* Workaround for fixed header block hero content, added mt-11 to the div */}
+          <div className="mt-11">{children}</div>
           <Footer />
         </AppProvider>
-        </body>
-      </html>
-    );
-  }
+      </body>
+    </html>
+  );
+}
