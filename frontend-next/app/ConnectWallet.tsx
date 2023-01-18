d37ef07a0c1c9b56e4a1475b53062151673f4210
Tolu Olugbesan <tolugbesan@gmail.com>
2023-01-18 14:54:32 +0100

    Minor refactoring and update to UI

--- frontend-next/app/ConnectWallet.tsx
+++ frontend-next/app/ConnectWallet.tsx
@@ -1,33 +1,36 @@
 "use client";
-import Link from "next/link"
-import React from "react"
-import { useContext } from "react"
+import Link from "next/link";
+import React from "react";
+import { useContext } from "react";
 import AppProvider, { AppContext } from "../context/AppContext";
+import { compressAddress } from "../utils/helper";
 
 function ConnectWallet() {
+  const { connected, account, connectWallet, error } = useContext(AppContext);
 
-  const { connected, account, connectWallet, error } = useContext(AppContext)
-
-  function compressAddress(address: string): string {
-    return address.slice(0, 5) + '...' + address.slice(-4);
-  }
+  // function compressAddress(address: string): string {
+  //   return address.slice(0, 5) + '...' + address.slice(-4);
+  // }
 
   return (
-      <>
-      {connected 
-        ?   <div className="flex items-center px-5 py-2 bg-transparent border border-gray-900 rounded-xl outline-none ring-2 ring-offset-2 ring-gray-900">
-                <img 
-                    className="object-cover w-5 h-5 rounded-full shrink-0 border" 
-                    src="https://landingfoliocom.imgix.net/store/collection/niftyui/images/featured-drops-marketplace/10/author.png" 
-                    alt="" 
-                />
-                <span title="" className="ml-2 text-sm font-medium text-gray-900">{compressAddress(account)}</span>
-            </div>
+    <>
+      {connected ? (
+        <div className="flex items-center px-5 py-2 bg-transparent border border-gray-900 rounded-xl outline-none ring-2 ring-offset-2 ring-gray-900">
+          <img
+            className="object-cover w-5 h-5 rounded-full shrink-0 border"
+            src="https://landingfoliocom.imgix.net/store/collection/niftyui/images/featured-drops-marketplace/10/author.png"
+            alt=""
+          />
+          <span title="" className="ml-2 text-sm font-medium text-gray-900">
+            {compressAddress(account)}
+          </span>
+        </div>
+      ) : (
         // <span>Connected With <b>{compressAddress(account)}</b></span>
-        : <a
-            href="#"
-            title=""
-            className="
+        <a
+          href="#"
+          title=""
+          className="
                 inline-flex
                 items-center
                 justify-center
@@ -47,15 +50,14 @@ function ConnectWallet() {
                 hover:bg-gray-900 hover:text-white
                 focus:bg-gray-900 focus:text-white
             "
-            role="button"
-            onClick={connectWallet}
-          >
-              Connect Wallet
-          </a>
-      }
-      
-      </>
-  )
+          role="button"
+          onClick={connectWallet}
+        >
+          Connect Wallet
+        </a>
+      )}
+    </>
+  );
 }
 
-export default ConnectWallet
+export default ConnectWallet;
