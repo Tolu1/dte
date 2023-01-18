6a43bdf1f48f2b23f11e9bce162b07424082186e
Tolu Olugbesan <tolugbesan@gmail.com>
2023-01-18 15:13:33 +0100

    Update default network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,8 @@
 import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import * as fs from 'fs';
-import * as path from 'path';
-import dotenv from 'dotenv';
+import * as fs from "fs";
+import * as path from "path";
+import dotenv from "dotenv";
 dotenv.config();
 
 // Sample Hardhat Task for printing accounts on the blockchain
@@ -33,7 +33,7 @@ async function copyAbis(src: string, dest: string) {
     if (fs.lstatSync(fullSrc).isDirectory()) {
       // Recursively copy the directory contents
       await copyAbis(fullSrc, fullDest);
-    } else if (file.endsWith('.json') && !file.endsWith('dbg.json')) {
+    } else if (file.endsWith(".json") && !file.endsWith("dbg.json")) {
       // Copy the file if it has a ".json" extension
       const data = fs.readFileSync(fullSrc);
       fs.writeFileSync(fullDest, data);
@@ -41,30 +41,37 @@ async function copyAbis(src: string, dest: string) {
   }
 }
 
-task('copy-abis', 'Copies the ABI files from the artifacts/contracts directory to the frontend-next/abis directory', async (taskArgs) => {
-  try {
-    // Copy the ABI files from the "artifacts/contracts" directory to the "frontend-next/abis" directory
-    await copyAbis(path.join(__dirname, 'artifacts', 'contracts'), path.join(__dirname, 'frontend-next', 'abis'));
-  } catch (error) {
-    console.error(error);
+task(
+  "copy-abis",
+  "Copies the ABI files from the artifacts/contracts directory to the frontend-next/abis directory",
+  async (taskArgs) => {
+    try {
+      // Copy the ABI files from the "artifacts/contracts" directory to the "frontend-next/abis" directory
+      await copyAbis(
+        path.join(__dirname, "artifacts", "contracts"),
+        path.join(__dirname, "frontend-next", "abis")
+      );
+    } catch (error) {
+      console.error(error);
+    }
   }
-});
+);
 
 const { API_URL, PRIVATE_KEY } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
-  defaultNetwork: "localhost",
+  defaultNetwork: "goerli",
   networks: {
     hardhat: {},
     localhost: {
-      url: "http://127.0.0.1:8545"
+      url: "http://127.0.0.1:8545",
     },
     goerli: {
-      url:API_URL,
-      accounts: [`0x${PRIVATE_KEY}`]
-    } 
-  }
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
 
 export default config;
