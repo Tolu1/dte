e068b615e7b0d475a2edcd861a30badb7581bf92
Tolu Olugbesan <tolugbesan@gmail.com>
2023-01-23 09:14:43 +0100

    Make homepage components invisible for now

--- frontend-next/pages/index.tsx
+++ frontend-next/pages/index.tsx
@@ -25,8 +25,8 @@ export default function Home({
       </Head>
       <main>
         <Hero latestDonationValue={latestDonationValue} />
-        <CharitySpotlight />
-        <Mission />
+        {/* <CharitySpotlight /> */}
+        {/* <Mission /> */}
       </main>
     </>
   );
